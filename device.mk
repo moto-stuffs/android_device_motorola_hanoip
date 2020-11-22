5571fd3634e05bbfe2bdb921d163cf89b11ffaa9
LuK1337 <priv.luk@gmail.com>
2020-11-22 03:14:32 +0100

    hanoip: overlay-lineage: Enable min_refresh_rate setting
    Change-Id: I2e16b99318003de819d84cab03f7baa33b9b6f2b

--- device.mk
+++ device.mk
@@ -68,6 +68,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/com.android.nfc_extras.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.android.nfc_extras.xml \
     frameworks/native/data/etc/com.nxp.mifare.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.nxp.mifare.xml
 
+# Overlays
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
+
 # Partitions
 PRODUCT_BUILD_SUPER_PARTITION := false
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
