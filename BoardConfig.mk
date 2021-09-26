67c5e73ffdf48450fd32b44a1d6af68eaec379a2
raghavt20 <raghavtandon2000@gmail.com>
2021-09-27 01:58:29 +0530

    hanoip: Inherit proprietary files
    Change-Id: If6e8af0917c6cb58654dd20ff327b9e8176cc06b

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -4,4 +4,10 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Inherit from sm6150-common
+include device/motorola/sm6150-common/BoardConfigCommon.mk
+
 DEVICE_PATH := device/motorola/hanoip
+
+# Inherit from proprietary files
+include vendor/motorola/hanoip/BoardConfigVendor.mk
