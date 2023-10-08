00a245e763bd59803b127d9a32608d650e87dc4f
Pranaya Deomani <pranayadeomani@proton.me>
2023-10-08 07:48:36 +0000

    hanoip: Add A/B configuration
    Change-Id: I9a0adf6e99bb1ffe009261d0abb1582080998853
    Signed-off-by: Vhmit <viktor.9630@protonmail.com>

--- device.mk
+++ device.mk
@@ -7,14 +7,22 @@
 # Inherit from sm6150-common
 $(call inherit-product, device/motorola/sm6150-common/common.mk)
 
+# A/B Updater
 AB_OTA_PARTITIONS += \
     system_ext \
     vbmeta_system \
     vendor_boot
 
+AB_OTA_POSTINSTALL_CONFIG += \
+    RUN_POSTINSTALL_vendor=true \
+    POSTINSTALL_PATH_vendor=bin/checkpoint_gc \
+    FILESYSTEM_TYPE_vendor=ext4 \
+    POSTINSTALL_OPTIONAL_vendor=true
+
 # Partitions
 PRODUCT_BUILD_SUPER_PARTITION := false
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
+PRODUCT_PACKAGES += checkpoint_gc 
 
 # Shipping API Level
 PRODUCT_SHIPPING_API_LEVEL := 30
