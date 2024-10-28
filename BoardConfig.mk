a69a92e9476f3d4924a37313fa7d1e317b9b96d5
raghavt20 <raghavtandon2000@gmail.com>
2024-10-28 10:33:08 -0300

    hanoip: Configure kernel flags
    Change-Id: Ia2faaecdbb0b08afefb8c8b3cec629948e45ba88

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -12,5 +12,13 @@ DEVICE_PATH := device/motorola/hanoip
 # Display
 TARGET_SCREEN_DENSITY := 420
 
+# Kernel
+BOARD_BOOT_HEADER_VERSION := 3
+TARGET_KERNEL_CONFIG := vendor/hanoip_defconfig
+
+# Kernel modules - Audio
+TARGET_MODULE_ALIASES += \
+    snd-soc-aw882xx.ko:snd_smartpa_aw882xx.ko
+
 # Inherit from proprietary files
 include vendor/motorola/hanoip/BoardConfigVendor.mk
