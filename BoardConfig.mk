b36a6c9c58f989618633c6f11c3425d2cb893c8c
Vhmit <viktor.9630@protonmail.com>
2024-10-31 11:08:09 -0300

    hanoip: Define assertions of flashall in fastbootd
    Change-Id: Icfaf5cba47ddc81f10798d75b62c8ea627d082af
    Signed-off-by: Vhmit <viktor.9630@protonmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -9,6 +9,10 @@ include device/motorola/sm6150-common/BoardConfigCommon.mk
 
 DEVICE_PATH := device/motorola/hanoip
 
+# Assertions
+TARGET_BOARD_INFO_FILE := $(DEVICE_PATH)/board-info.txt
+TARGET_OTA_ASSERT_DEVICE := hanoip
+
 # Display
 TARGET_SCREEN_DENSITY := 420
 
